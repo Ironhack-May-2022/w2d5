6de9554901d0cc9d1d01a809945d80e6176d11aa
Jan Redmann <janredmann1@gmail.com>
2022-06-10 10:46:52 +0200

    add background, player and jump

--- p5-game/js/game.js
+++ p5-game/js/game.js
@@ -1,2 +1,22 @@
 class Game {
+	constructor() {
+		this.background = new Background()
+		this.backgroundImages = []
+		this.player = new Player()
+	}
+	preload() {
+		this.backgroundImages = [
+			{ src: loadImage('assets/background/plx-1.png'), x: 0, speed: 0 },
+			{ src: loadImage('assets/background/plx-2.png'), x: 0, speed: 1 },
+			{ src: loadImage('assets/background/plx-3.png'), x: 0, speed: 2 },
+			{ src: loadImage('assets/background/plx-4.png'), x: 0, speed: 3 },
+			{ src: loadImage('assets/background/plx-5.png'), x: 0, speed: 4 }
+		]
+		this.playerImage = loadImage('assets/player/bb8.gif')
+	}
+	draw() {
+		clear()
+		this.background.draw()
+		this.player.draw()
+	}
 }
--- p5-game/js/main.js
+++ p5-game/js/main.js
@@ -1,11 +1,20 @@
+const game = new Game()
+
+// all these functions will be called by the P5 library
 function preload() {
+	game.preload()
 }
 
 function setup() {
+	createCanvas(600, 600)
 }
 
 function draw() {
+	game.draw()
 }
 
 function keyPressed() {
+	if (keyCode === 32) {
+		game.player.jump()
+	}
 }
